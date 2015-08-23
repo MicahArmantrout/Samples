a3f717d95a7e82e923f86fb96e6592d9b7001194
Carl Robinson <Carl.Robinson@zenilogix.com>
2015-08-23 12:15:55 -0400

    Added sample to demonstrate a simple way to handle exceptions in Razor

---
+++ RazorMVC/Exceptions/Exceptions/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exceptions.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// The home page for our sample site
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// A second page on our site which throws an exception
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Page2()
+        {
+            // Demonstrate the exception handling process by throwing an exception in response to a request for this page
+            throw new Exception("A demonstration exception");
+        }
+
+    }
+}
---
+++ RazorMVC/Exceptions/Exceptions/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Exceptions
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(Object sender, EventArgs e)
+        {
+            // See http://stackoverflow.com/questions/13905164/how-to-make-custom-error-pages-work-in-asp-net-mvc-4
+            // for additional context on use of this technique
+
+            var exception = Server.GetLastError();
+            if (exception != null)
+            {
+                // This will invoke our error page, passing the exception message via querystring parameter
+                // Note that we chose to use Server.TransferRequest, which is only supported in IIS 7 and above.
+                // As an alternative, Response.Redirect could be used instead.
+                // Server.Transfer does not work (see https://support.microsoft.com/en-us/kb/320439 )
+                Server.TransferRequest("~/Error?Message=" + exception.Message);
+            }
+
+            // TODO: Investigate the following:
+
+            //try
+            //{
+            //    // This is to stop a problem where we were seeing "gibberish" in the
+            //    // chrome and firefox browsers
+            //    HttpApplication app = sender as HttpApplication;
+            //    app.Response.Filter = null;
+            //}
+            //catch
+            //{
+            //}
+        }
+    }
+}
